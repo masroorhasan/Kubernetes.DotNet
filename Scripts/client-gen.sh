0952da4b5f7535b2e532939d431cee2740875ca4
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-10-28 01:06:44 -0700

    rename some dirs in script;

--- Scripts/client-gen.sh
+++ Scripts/client-gen.sh
@@ -11,17 +11,15 @@ rm -rf "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}"
 
 # download openapi k8s code gen preprocessor
 git clone https://github.com/kubernetes-client/gen.git "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}" --depth=1
-mkdir swagger
-python "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}/openapi/preprocess_spec.py" "${KUBERNETES_BRANCH}" "${CURRENT_DIR}/swagger/swagger.json"
+mkdir Swagger
+python "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}/openapi/preprocess_spec.py" "${KUBERNETES_BRANCH}" "${CURRENT_DIR}/Swagger/swagger.json"
 
 # generate with appropriate config
-if [ -f "${CURRENT_DIR}/src/${KUBERNETES_SRC_DIR}" ]; then
-    swagger-codegen generate -i ${CURRENT_DIR}/swagger/swagger.json -l csharp -o ${CURRENT_DIR} -c ${CURRENT_DIR}/scripts/update-config.json
+if [ -f "${CURRENT_DIR}/Kubernetes/src/${KUBERNETES_SRC_DIR}" ]; then
+    swagger-codegen generate -i ${CURRENT_DIR}/Swagger/swagger.json -l csharp -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/update-config.json
 else
-    swagger-codegen generate -i ${CURRENT_DIR}/swagger/swagger.json -l csharp -o ${CURRENT_DIR} -c ${CURRENT_DIR}/scripts/init-config.json
+    swagger-codegen generate -i ${CURRENT_DIR}/Swagger/swagger.json -l csharp -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/init-config.json
 fi
 
 # clean up
 rm -rf "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}"
-
-
