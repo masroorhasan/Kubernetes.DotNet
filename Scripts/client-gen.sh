1df61a3165b04878758ef38b5e3b1958161aefae
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-19 15:25:11 -0800

    update client generation script;

--- Scripts/client-gen.sh
+++ Scripts/client-gen.sh
@@ -5,6 +5,7 @@ CURRENT_DIR=`pwd`
 KUBERNETES_BRANCH="release-1.7"
 OPENAPI_CLIENT_GEN_DIR="openapi-client-gen"
 KUBERNETES_SRC_DIR="Kubernetes.DotNet"
+CLIENT_LANG="csharp"
 
 # remove
 rm -rf "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}"
@@ -12,13 +13,13 @@ rm -rf "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}"
 # download openapi k8s code gen preprocessor
 git clone https://github.com/kubernetes-client/gen.git "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}" --depth=1
 mkdir Swagger
-python "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}/openapi/preprocess_spec.py" "${KUBERNETES_BRANCH}" "${CURRENT_DIR}/Swagger/swagger.json"
+python "${CURRENT_DIR}/${OPENAPI_CLIENT_GEN_DIR}/openapi/preprocess_spec.py" "${CLIENT_LANG}" "${KUBERNETES_BRANCH}" "${CURRENT_DIR}/Swagger/swagger.json"
 
 # generate with appropriate config
 if [ -f "${CURRENT_DIR}/Kubernetes/src/${KUBERNETES_SRC_DIR}" ]; then
-    swagger-codegen generate -i ${CURRENT_DIR}/Swagger/swagger.json -l csharp -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/update-config.json
+    swagger-codegen generate -i ${CURRENT_DIR}/Swagger/swagger.json -l ${CLIENT_LANG} -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/update-config.json
 else
-    swagger-codegen generate -i ${CURRENT_DIR}/Swagger/swagger.json -l csharp -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/init-config.json
+    swagger-codegen generate -i ${CURRENT_DIR}/Swagger/swagger.json -l ${CLIENT_LANG} -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/init-config.json
 fi
 
 # clean up
