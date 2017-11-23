cb6785bc5d45c9d2db78891f51caabc8d5d8126b
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-23 14:27:13 -0800

    update dockerfile to include k8s branch version; update gen management script;

--- Scripts/client-gen.sh
+++ Scripts/client-gen.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # constants
+KUBERNETES_BRANCH=$K8S_BRANCH_VERSION
 CURRENT_DIR=`pwd`
-KUBERNETES_BRANCH="release-1.7"
 OPENAPI_CLIENT_GEN_DIR="openapi-client-gen"
 KUBERNETES_SRC_DIR="Kubernetes.DotNet"
 CLIENT_LANG="csharp"
+
+# Trace
+echo "Generating C# client for Kubernetes API:${KUBERNETES_BRANCH}."
 
 # create dir for swagger spec
 mkdir Swagger
@@ -22,8 +25,10 @@ wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagge
 
 # generate with appropriate config
 if [ -f "${CURRENT_DIR}/Kubernetes/src/${KUBERNETES_SRC_DIR}" ]; then
+    echo "Updating K8s API."
     java -jar swagger-codegen-cli.jar generate -i ${CURRENT_DIR}/Swagger/swagger.json -l ${CLIENT_LANG} -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/update-config.json
 else
+    echo "Creating K8s API."
     java -jar swagger-codegen-cli.jar generate -i ${CURRENT_DIR}/Swagger/swagger.json -l ${CLIENT_LANG} -o ${CURRENT_DIR}/Kubernetes -c ${CURRENT_DIR}/Scripts/init-config.json
 fi
 
