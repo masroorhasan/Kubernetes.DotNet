a0abad13a179c07f63b3b729e1cfdfc914490cc1
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-24 11:29:09 -0800

    manually nuget restore;

--- Kubernetes/build.bat
+++ Kubernetes/build.bat
@@ -9,14 +9,20 @@ if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
 
-set nuget=
-if "%nuget%" == "" (
-	set nuget=nuget
-)
+REM Package restore
+call %NuGet% restore Kubernetes\src\Kubernetes.DotNet\packages.config -OutputDirectory %cd%\packages -NonInteractive
+REM call %NuGet% restore GoogleAnalyticsTracker.WP7\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
-rem ************** Build ************** 
-"%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" Kubernetes\Kubernetes.DotNet.sln /p:Configuration="%config%"
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Kubernetes\Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 rem ************** Pack **************
 mkdir Build
 %nuget% pack "Kubernetes\src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+
+REM Package
+mkdir Build
+mkdir Build\net40
+call %NuGet% pack "Kubernetes\src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -symbols -o Build\net40 -p Configuration=%config% %version%
+copy Kubernetes\src\Kubernetes.DotNet\bin\%config%\*.dll Build\net40
+copy Kubernetes\src\Kubernetes.DotNet\bin\%config%\*.pdb Build\net40
