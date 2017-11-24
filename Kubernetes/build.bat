b134b2d64349ffa750b9bd849c688453c8d758ff
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-23 18:52:07 -0800

    Fix path issues in build.bat

--- Kubernetes/build.bat
+++ Kubernetes/build.bat
@@ -16,12 +16,12 @@ if "%nuget%" == "" (
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-%nuget% install src\Kubernetes.DotNet\packages.config -o packages
+%nuget% install Kubernetes\src\Kubernetes.DotNet\packages.config -o packages
 if not exist ".\bin" mkdir bin
 copy packages\BouncyCastle.1.8.1\lib\BouncyCastle.Crypto.dll
 copy packages\YamlDotNet.4.2.2\lib\net35\YamlDotNet.dll
 copy packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
 copy packages\RestSharp.105.1.0\lib\net45\RestSharp.dll bin\RestSharp.dll
 
-%nuget% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.nuspec" -NoPackageAnalysis -verbosity detailed -o bin -Version %version% -p Configuration="%config%"
+%nuget% pack "Kubernetes\src\Kubernetes.DotNet\Kubernetes.DotNet.nuspec" -NoPackageAnalysis -verbosity detailed -o bin -Version %version% -p Configuration="%config%"
 
