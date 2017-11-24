2771041e455c451cceff31d671b59e6bb5ec724f
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-23 19:52:48 -0800

    testing with simple csproj build;

--- Kubernetes/build.bat
+++ Kubernetes/build.bat
@@ -15,16 +15,21 @@ if "%nuget%" == "" (
 )
 
 REM install packages
-%nuget% install src\Kubernetes.DotNet\packages.config -o packages
-if not exist ".\bin" mkdir bin
-copy packages\BouncyCastle.1.8.1\lib\BouncyCastle.Crypto.dll bin\BouncyCastle.Crypto.dll
-copy packages\YamlDotNet.4.2.2\lib\net35\YamlDotNet.dll bin\YamlDotNet.dll
-copy packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
-copy packages\RestSharp.105.1.0\lib\net45\RestSharp.dll bin\RestSharp.dll
+REM %nuget% install src\Kubernetes.DotNet\packages.config -o packages
+REM if not exist ".\bin" mkdir bin
+REM copy packages\BouncyCastle.1.8.1\lib\BouncyCastle.Crypto.dll bin\BouncyCastle.Crypto.dll
+REM copy packages\YamlDotNet.4.2.2\lib\net35\YamlDotNet.dll bin\YamlDotNet.dll
+REM copy packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
+REM copy packages\RestSharp.105.1.0\lib\net45\RestSharp.dll bin\RestSharp.dll
 
 REM build solution
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+REM Build
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM create package
-%nuget% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.nuspec" -NoPackageAnalysis -verbosity detailed -o bin -Version %version% -p Configuration="%config%"
+REM %nuget% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.nuspec" -NoPackageAnalysis -verbosity detailed -o bin -Version %version% -p Configuration="%config%"
+
+REM package
+mkdir Build
+call %nuget% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -symbols -o Build -p Configuration=%config% %version%
 
