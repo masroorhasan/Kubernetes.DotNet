daea08440efad190f1bedc5618837e2a81acb5fb
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-23 18:48:13 -0800

    Update build.bat

--- Kubernetes/build.bat
+++ Kubernetes/build.bat
@@ -4,7 +4,7 @@ if "%config%" == "" (
    set config=Release
 )
  
-set version=1.0.0
+set version=0.7.0
 if not "%PackageVersion%" == "" (
    set version=%PackageVersion%
 )
@@ -14,16 +14,14 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\Terrarium.Sdk.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-if not exist ".\nuget.exe" powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', '.\nuget.exe')"
-.\nuget.exe install src\Kubernetes.DotNet\packages.config -o packages
-
+%nuget% install src\Kubernetes.DotNet\packages.config -o packages
 if not exist ".\bin" mkdir bin
-
 copy packages\BouncyCastle.1.8.1\lib\BouncyCastle.Crypto.dll
 copy packages\YamlDotNet.4.2.2\lib\net35\YamlDotNet.dll
 copy packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll bin\Newtonsoft.Json.dll
 copy packages\RestSharp.105.1.0\lib\net45\RestSharp.dll bin\RestSharp.dll
-%CSCPATH%\csc  /reference:bin\Newtonsoft.Json.dll;bin\RestSharp.dll;bin\BouncyCastle.Crypto.dll;YamlDotNet.dll;System.ComponentModel.DataAnnotations.dll  /target:library /out:bin\Kubernetes.DotNet.dll /recurse:src\Kubernetes.DotNet\*.cs /doc:bin\Kubernetes.DotNet.xml
+
+%nuget% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.nuspec" -NoPackageAnalysis -verbosity detailed -o bin -Version %version% -p Configuration="%config%"
 
