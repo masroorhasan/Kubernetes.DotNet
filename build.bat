37943fc6d8150fc7341dca7f28940871cdd53fa3
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-25 20:11:59 -0800

    update build path;

--- build.bat
+++ build.bat
@@ -10,19 +10,19 @@ if not "%PackageVersion%" == "" (
 )
 
 REM Package restore
-call %NuGet% restore Kubernetes\src\Kubernetes.DotNet\packages.config -OutputDirectory Kubernetes\packages -NonInteractive
-call %NuGet% restore Kubernetes\src\Kubernetes.DotNet.Test\packages.config -OutputDirectory Kubernetes\packages -NonInteractive
+call %NuGet% restore src\Kubernetes.DotNet\packages.config -OutputDirectory packages -NonInteractive
+call %NuGet% restore src\Kubernetes.DotNet.Test\packages.config -OutputDirectory packages -NonInteractive
 
 REM Build
-"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Kubernetes\Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+"%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" Kubernetes.DotNet.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 rem ************** Pack **************
 mkdir Build
-%nuget% pack "Kubernetes\src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
 
 REM Package
 mkdir Build
 mkdir Build\net40
-call %NuGet% pack "Kubernetes\src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -symbols -o Build\net40 -p Configuration=%config% %version%
-copy Kubernetes\src\Kubernetes.DotNet\bin\%config%\*.dll Build\net40
-copy Kubernetes\src\Kubernetes.DotNet\bin\%config%\*.pdb Build\net40
+call %NuGet% pack "src\Kubernetes.DotNet\Kubernetes.DotNet.csproj" -symbols -o Build\net40 -p Configuration=%config% %version%
+copy src\Kubernetes.DotNet\bin\%config%\*.dll Build\net40
+copy src\Kubernetes.DotNet\bin\%config%\*.pdb Build\net40
