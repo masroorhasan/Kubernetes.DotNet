3f0e24203cd259a6f6813fef9faa785ebaa746dd
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-28 23:40:17 -0800

    add travis deployment script; (#14)

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+MajorVersion=$1
+MinorVersion=$2
+BuildVersion=$3
+ApiKey=$4
+
+mono nuget.exe pack ./src/Kubernetes.DotNet/Kubernetes.DotNet.nuspec -Version $MajorVersion.$MinorVersion.$BuildVersion -p Configuration=Release -Verbosity detailed
+mono nuget.exe setApiKey $ApiKey -Source https://www.nuget.org -Verbosity quiet
+mono nuget.exe push Kubernetes.DotNet.$MajorVersion.$MinorVersion.$BuildVersion.nupkg -Source https://www.nuget.org/api/v2/package
