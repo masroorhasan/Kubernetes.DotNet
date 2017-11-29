c7eb8ec525d758a3b8dd9c8fea53f9adde29289b
Masroor Hasan <masroor.hasan.n@gmail.com>
2017-11-29 00:09:41 -0800

    reference travis env variable from deploy script directly;

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
-MajorVersion=$1
-MinorVersion=$2
-BuildVersion=$3
-ApiKey=$4
+MajorVersion=$MAJOR_VERSION_NUMBER
+MinorVersion=$MINOR_VERSION_NUMBER
+BuildVersion=$TRAVIS_BUILD_NUMBER
+ApiKey=$NUGET_API_KEY
 
 mono nuget.exe pack ./src/Kubernetes.DotNet/Kubernetes.DotNet.nuspec -Version $MajorVersion.$MinorVersion.$BuildVersion -p Configuration=Release -Verbosity detailed
 mono nuget.exe setApiKey $ApiKey -Source https://www.nuget.org -Verbosity quiet
