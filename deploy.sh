578e3d89f6b79020e75d521d571c61fbd638083c
Masroor Hasan <mhasan@mux.com>
2018-10-09 08:59:39 -0700

    update travis deploy script (#20)
    * travis deploy from script
    
    * update readme

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 MajorVersion=$MAJOR_VERSION_NUMBER
 MinorVersion=$MINOR_VERSION_NUMBER
 BuildVersion=$TRAVIS_BUILD_NUMBER
 ApiKey=$NUGET_API_KEY
 
+echo "Packing nuspec package for $MajorVersion.$MinorVersion.$BuildVersion"
 mono nuget.exe pack ./src/Kubernetes.DotNet/Kubernetes.DotNet.nuspec -Version $MajorVersion.$MinorVersion.$BuildVersion -p Configuration=Release -Verbosity detailed
 mono nuget.exe setApiKey $ApiKey -Source https://www.nuget.org -Verbosity quiet
+
+echo "Pushing package to nuget"
 mono nuget.exe push Kubernetes.DotNet.$MajorVersion.$MinorVersion.$BuildVersion.nupkg -Source https://www.nuget.org/api/v2/package
+echo "Done"
